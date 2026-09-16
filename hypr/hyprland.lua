b96373cc56e4818ef45291bdc06b593ee0d4cabc
dumpstertree <dumpstertree@gmail.com>
2026-09-16 16:32:57 -0400

    Update hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -28,6 +28,7 @@ local menu        = "hyprlauncher"
 
  hl.on("hyprland.start", function () 
    hl.exec_cmd("hyprpaper & hypridle & waybar")
+   hl.exec_cmd("hyprpm reload")
 end)
 
 -------------------------------
